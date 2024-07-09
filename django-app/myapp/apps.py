d247d46e1afd3aa0876aa9fcbbf6dfedfbd9a98c
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-07-09 12:25:55 -0400

    commit

--- django-app/myapp/apps.py
+++ django-app/myapp/apps.py
@@ -1,4 +1,4 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # type: ignore
 
 class MyAppConfig(AppConfig):
     name = 'myapp'
--- django-app/myproject/wsgi.py
+++ django-app/myproject/wsgi.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
