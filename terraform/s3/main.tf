71bd530709b255e99f96b31a883bbdf0b15a4ce7
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-07-12 18:09:42 -0400

    main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "mongodb_backup" {
   bucket = "mongodb-backup-bucket"
+}
+
+resource "aws_s3_bucket_acl" "mongodb_backup_acl" {
+  bucket = aws_s3_bucket.mongodb_backup.id
   acl    = "private"  # Adjust as necessary
 }
 
@@ -9,11 +13,6 @@ resource "aws_s3_bucket_versioning" "mongodb_backup_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
-}
-
-resource "aws_s3_bucket_acl" "mongodb_backup_acl" {
-  bucket = aws_s3_bucket.mongodb_backup.id
-  acl    = "public-read"  # Adjust as necessary
 }
 
 resource "aws_s3_bucket_tagging" "mongodb_backup_tagging" {
